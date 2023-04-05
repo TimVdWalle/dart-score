4234a6668909792f26f7e3a0f2bf5022f6964655
timvandewalle <tim@vandewalle.mobi>
2023-04-05 22:43:45 +0200

    Add initial vue compontent layout

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -15,7 +15,7 @@
         @vite(['resources/js/app.js', "resources/js/Pages/{$page['component']}.vue"])
         @inertiaHead
     </head>
-    <body class="font-sans antialiased">
+    <body class="font-sans antialiased bg-gg_grey_dark">
         @inertia
     </body>
 </html>
--- tailwind.config.js
+++ tailwind.config.js
@@ -10,9 +10,43 @@ module.exports = {
     ],
 
     theme: {
+        fontFamily: {
+
+            display: ['Montserrat','sans-serif'],
+            body: ['"Open Sans"', 'sans-serif'],
+            sans: ['"Open Sans"', 'sans-serif'],
+        },
+        fontWeight: {
+            'thin': 100,
+            'extralight': 200,
+            'light': 300,
+            'regular': 400,
+            'medium': 500,
+            'semibold': 600,
+            'bold': 700,
+            'extrabold': 800,
+            'black': 800,
+        },
+
+        colors: {
+            transparent: 'transparent',
+            current: 'currentColor',
+            'gg_red': '#DE163B',
+            'gg_green': '#38A21F',
+
+            'white': '#FFFFFF',
+
+            'gg_grey_darker': '#0A0B0E',
+            'gg_grey_dark': '#181C23',
+            'gg_grey_light': '#292E38',
+            'gg_grey_lighter': '#363D4A',
+        },
         extend: {
             fontFamily: {
-                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+
+                'display': ['Montserrat','sans-serif'],
+                'body': ['"Open Sans"', 'sans-serif'],
+                sans: ['"Open Sans"', 'sans-serif'],
             },
         },
     },
