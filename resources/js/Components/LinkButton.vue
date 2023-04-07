231bcb23fe44498a6190b861199a6e1227b406d8
timvandewalle <tim@vandewalle.mobi>
2023-04-07 22:49:31 +0200

    ✨ Add button component

---
+++ resources/js/Components/LinkButton.vue
@@ -0,0 +1,27 @@
+<template>
+    <Link :href="route(this.href)">
+        <Button>
+            <template #icon>
+                <slot name="icon"></slot>
+            </template>
+
+            <template #title>
+                <slot name="title" />
+            </template>
+
+            <slot />
+        </Button>
+    </Link>
+</template>
+
+<script setup>
+import {  Link } from '@inertiajs/vue3';
+import Button from "@/Components/Button.vue";
+
+const props = defineProps({
+    href: String
+});
+
+console.log(props.href);
+</script>
+
