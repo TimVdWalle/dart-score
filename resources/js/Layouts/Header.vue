491903d5e5d74dfa98040032bf726696f5090dc3
timvandewalle <tim@vandewalle.mobi>
2023-04-07 22:50:15 +0200

    ⚡ Update UI : refactor classes for home / applayout

--- resources/js/Layouts/Header.vue
+++ resources/js/Layouts/Header.vue
@@ -1,16 +1,13 @@
 <template>
     <nav class="bg-gg_grey_darker">
-        <div class="flex max-w-6xl mx-auto ">
+        <div class="flex xl:max-w-6xl xl:mx-auto ml-16">
             <div class="py-10 w-full text-white font-display italic font-black text-xl logo">
                 <span>Dart</span><span>Score</span>
             </div>
         </div>
     </nav>
-    test
-    <font-awesome-icon :icon="faUser" />    test
 </template>
 
 <script setup>
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import { faUser } from '@fortawesome/free-solid-svg-icons'
+
 </script>
