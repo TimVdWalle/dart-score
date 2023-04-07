ef0477eb206b78c6e83350f98e593c366ac0998e
timvandewalle <tim@vandewalle.mobi>
2023-04-07 22:50:26 +0200

    ✨ Add game init page

---
+++ resources/js/Pages/Game/Init.vue
@@ -0,0 +1,13 @@
+<script setup>
+import AppLayout from "@/Layouts/AppLayout.vue";
+import { Head, Link } from '@inertiajs/vue3';
+import LinedTitle from "@/Elements/LinedTitle.vue";
+</script>
+
+<template>
+    <Head title="Game" />
+
+    <AppLayout classes="xl:max-w-6xl xl:mx-auto ml-16">
+        <lined-title>Spelers</lined-title>
+    </AppLayout>
+</template>
