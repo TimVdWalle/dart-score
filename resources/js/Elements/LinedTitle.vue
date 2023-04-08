bb573d9858489303d7ad0a0e5e736efc10adf612
timvandewalle <tim@vandewalle.mobi>
2023-04-08 16:10:25 +0200

    🚧 WIP

--- resources/js/Elements/LinedTitle.vue
+++ resources/js/Elements/LinedTitle.vue
@@ -2,15 +2,15 @@
     <div class="">
         <div class="flex items-center py-4">
             <!-- The left line -->
-            <div class="flex-grow h-1 max-w-[30px] w-30 bg-gg_grey_medium"></div>
+            <div class="flex-grow h-1 max-w-[30px] w-30 bg-grey_medium"></div>
 
             <!-- Your text here -->
-            <span class="flex-shrink text-2xl px-4 text-gg_red uppercase font-display font-semibold">
+            <span class="flex-shrink text-2xl px-4 text-red uppercase font-display font-semibold">
                 <slot />
             </span>
 
             <!-- The right line -->
-            <div class="flex-grow h-1 bg-gg_grey_medium"></div>
+            <div class="flex-grow h-1 bg-grey_medium"></div>
         </div>
     </div>
 </template>
