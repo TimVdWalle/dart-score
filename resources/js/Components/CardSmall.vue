5da4bab473364ef676175883df352abe685607d7
timvandewalle <tim@vandewalle.mobi>
2023-04-08 16:36:30 +0200

    ✨ Add button to add players

--- resources/js/Components/CardSmall.vue
+++ resources/js/Components/CardSmall.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="flex w-80 p-5 bg-grey_medium gap-x-4 text-white font-display font-bold uppercase text-lg px-5 rounded hover:cursor-pointer hover:bg-red group" >
+    <div class="flex min-w-80 my-6 p-5 bg-grey_medium gap-x-4 text-white font-display font-bold uppercase text-lg px-5 rounded hover:cursor-pointer hover:bg-red group" >
         <div v-if="$slots.icon" class="px-2">
             <slot name="icon"/>
         </div>
--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -1,27 +1,21 @@
 <script setup>
 import AppLayout from "@/Layouts/AppLayout.vue";
 import { Head, Link } from '@inertiajs/vue3';
-import Button from "@/Components/Button.vue";
+import Button from "@/Components/CardSmall.vue";
 import LinkButton from "@/Components/LinkButton.vue";
 
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import {faBullseye} from "@fortawesome/free-solid-svg-icons";
+import {faCircleUser} from "@fortawesome/free-solid-svg-icons";
+
 </script>
 
 <template>
     <Head title="Home" />
 
    <AppLayout classes="flex justify-center items-center h-[70vh]">
-        <LinkButton href="game.init">
-            <template #icon>
-                <font-awesome-icon :icon="faBullseye" class="fa-3x text-white"/>
-            </template>
 
-            <template #title>
-                dart
-            </template>
-
-            start spel
-        </LinkButton>
+       <Link :href="route('game.init')">
+            <button class="button big red">start spel</button>
+       </Link>
    </AppLayout>
 </template>
