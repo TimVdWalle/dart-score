8e43b93623e6d8a9ce9956f2e2ba3940a4fc2046
timvandewalle <tim@vandewalle.mobi>
2023-04-08 21:51:26 +0200

    ✨ Add tabgroup component

---
+++ resources/js/Components/TabGroup.vue
@@ -0,0 +1,18 @@
+<script setup>
+
+const props = defineProps({
+    options: Array
+});
+
+console.log(props.options)
+</script>
+
+<template>
+    <div class="flex gap-x-2">
+        <div v-for="option in options"
+             :class="['bg-grey_light', {'bg-red': option.selected}]"
+             class="p-3 flex-grow rounded flex items-center justify-center uppercase text-white font-semibold hover:bg-red hover:cursor-pointer">
+            {{option.value}}
+        </div>
+    </div>
+</template>
