ed755e455a69c7de4451eeebd1d7f444fd4dfc8e
timvandewalle <tim@vandewalle.mobi>
2023-04-08 23:22:04 +0200

    🔨 Refactor layout for players list

--- resources/js/Components/CardSmall.vue
+++ resources/js/Components/CardSmall.vue
@@ -9,7 +9,7 @@ const props = defineProps({
 
 <template>
     <div
-        class="flex relative min-w-80 my-6 p-5 bg-grey_medium gap-x-4 text-white font-display font-bold uppercase text-lg px-5 rounded fhover:cursor-pointer fhover:bg-red group">
+        class="flex w-full relative min-w-80 fmy-6 p-5 bg-grey_medium gap-x-4 text-white font-display font-bold uppercase text-lg px-5 rounded fhover:cursor-pointer fhover:bg-red group">
         <div v-if="closeButton" @click="$emit('close')" class="absolute top-0 right-0 flex p-2">
             <font-awesome-icon :icon="faXmark" class="fa-lg text-grey_lighter hover:text-grey_lightest hover:cursor-pointer" />
         </div>
