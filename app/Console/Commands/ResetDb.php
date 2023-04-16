390404a5ff98ae5590db61825181ccd330bd3176
timvandewalle <tim@vandewalle.mobi>
2023-04-16 11:51:06 +0200

    ✨ Add command to clean up testdata from db

---
+++ app/Console/Commands/ResetDb.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Game\Game;
+use App\Models\Game\Player;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\DB;
+
+/**
+ *
+ */
+class ResetDb extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'db:reset';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Reset db: clear all players, games, and games_players tables';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle(): void
+    {
+        $this->info("truncating tables: players and games");
+        Player::query()->truncate();
+        Game::query()->truncate();
+        DB::table('game_player')->truncate();
+
+        $this->info("truncating done!");
+    }
+}
