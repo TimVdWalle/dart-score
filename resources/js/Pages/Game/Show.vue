035bf035e67b017f3619312f5f44d85da0c20be1
timvandewalle <tim@vandewalle.mobi>
2023-04-16 13:59:33 +0200

    ⚡ Update layout for show page

--- resources/js/Pages/Game/Show.vue
+++ resources/js/Pages/Game/Show.vue
@@ -1,17 +1,19 @@
 <script setup>
 import {ref} from 'vue';
 import {Head} from '@inertiajs/vue3';
-import {Link} from '@inertiajs/vue3';
-
+import Players from "@/Components/Game/Players.vue";
 
 import GameLayout from "@/Layouts/GameLayout.vue";
+import Keyboard from "@/Components/Game/Keyboard.vue";
 </script>
 
 <template>
     <Head title="Game"/>
 
-    <GameLayout classes=" bg-white">
+    <GameLayout classes=" ">
+        <Players class="flex h-[25vh] min-h-[150px] bg-white justify-center items-center bg-white"/>
 
+        <Keyboard class="flex mt-3 fixed bottom-0 w-full h-[35vh] min-h-[250px] bg-white justify-center items-center bg-white" />
 
     </GameLayout>
 </template>
