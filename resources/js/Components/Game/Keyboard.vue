0deeb6606f192025ee1c6297c781f0c989f04ef0
timvandewalle <tim@vandewalle.mobi>
2023-04-16 13:58:12 +0200

    ✨ Add keyboard component

---
+++ resources/js/Components/Game/Keyboard.vue
@@ -0,0 +1,14 @@
+<script setup>
+import {FontAwesomeIcon} from '@fortawesome/vue-fontawesome'
+import {faXmark} from "@fortawesome/free-solid-svg-icons";
+
+const props = defineProps({
+    closeButton: Boolean
+})
+</script>
+
+<template>
+<div>
+    players
+</div>
+</template>
