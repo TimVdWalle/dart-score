899477987b5c0ca464825531ec95eab54f65951d
timvandewalle <tim@vandewalle.mobi>
2023-04-16 16:47:02 +0200

    Add content for faq page

---
+++ resources/js/Pages/Faq.vue
@@ -0,0 +1,16 @@
+Veelgestelde vragen
+
+Wat is DartScore?
+DartScore is een web-applicatie die is ontworpen om u te helpen bij het bijhouden van scores voor een spelletje darten. Met DartScore kunt u eenvoudig de scores voor elke speler invoeren, en de app zal automatisch de resterende scores berekenen en de volgende targets suggereren.
+
+Hoe gebruik ik DartScore?
+Het gebruik van DartScore is eenvoudig! Ga gewoon naar de website en selecteer het aantal spelers dat u wilt bijhouden. Voer vervolgens de score voor elke speler in na elke beurt, en DartScore doet de rest. U kunt ook de spelinstellingen aanpassen om overeen te komen met uw voorkeurs speltype en regels.
+
+Is DartScore gratis?
+Ja, DartScore is volledig gratis te gebruiken. Er zijn geen verborgen kosten of kosten en u hoeft zelfs geen account aan te maken om het te gebruiken.
+
+Wat is het verschil tussen DartScore en andere darts-scorekeeping-apps?
+DartScore is ontworpen om eenvoudig, gebruiksvriendelijk en aanpasbaar te zijn. In tegenstelling tot andere apps die mogelijk complexe functies hebben of u verplichten om te betalen voor premium functies, biedt DartScore alle basisfunctionaliteit voor scorekeeping die u nodig heeft zonder enige extra fratsen. Daarnaast kan DartScore worden gebruikt op elk apparaat met een internetverbinding, zodat u het op uw telefoon, tablet of computer kunt gebruiken.
+
+Waarom heet het "DartScore" in plaats van "DartsScore"?
+De naam "DartScore" is gekozen omdat het eenvoudig, beknopt en nauwkeurig de functie van de app weergeeft. "DartsScore" is daarentegen minder duidelijk en kan mogelijk verwarrend zijn voor gebruikers. Bovendien is "DartScore" grammaticaal correct, terwijl "DartsScore" dat niet is. Bij het gebruik van een zelfstandig naamwoord als samenstelling wordt meestal de enkelvoudsvorm van het zelfstandig naamwoord gebruikt. Daarom is "DartScore" de juiste en voorkeurs-optie voor de domeinnaam van een web-applicatie die de score van een darts-spel bijhoudt.
