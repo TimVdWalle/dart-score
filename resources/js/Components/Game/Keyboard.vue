e4cb5541fefdaf3e594e60135d17352d1a9fda91
timvandewalle <tim@vandewalle.mobi>
2023-04-16 13:58:34 +0200

    ✨ Add keys to keyboard

--- resources/js/Components/Game/Keyboard.vue
+++ resources/js/Components/Game/Keyboard.vue
@@ -8,7 +8,13 @@ const props = defineProps({
 </script>
 
 <template>
-<div>
-    players
-</div>
+    <div class="grid grid-cols-3 gap-1 bg-grey_lighterer p-1">
+        <div
+            v-for="number in [1,2,3,4,5,6,7,8,9, 0, 'del', 'undo']"
+            class="flex justify-center items-center bg-grey_medium h-full w-full font-display text-2xl rounded-lg hover:bg-blue-dark"
+        >
+            {{number}}
+        </div>
+    </div>
+
 </template>
