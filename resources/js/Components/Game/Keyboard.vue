c5ce182fe384cf9e33f8a2a89c2e825184a432d2
Tim Vande Walle <tim@vandewalle.mobi>
2023-11-05 13:27:27 +0100

    ✨ Add code to keyboard to handle button clicks

--- resources/js/Components/Game/Keyboard.vue
+++ resources/js/Components/Game/Keyboard.vue
@@ -1,19 +1,34 @@
 <script setup>
+import {ref} from 'vue';
 import {FontAwesomeIcon} from '@fortawesome/vue-fontawesome'
 import {faXmark} from "@fortawesome/free-solid-svg-icons";
+import KeyboardButton from "@/Components/Game/KeyboardButton.vue";
 
 const props = defineProps({
     closeButton: Boolean
 })
+
+const keyboardValue = ref([]);
+keyboardValue.value = 0;
+
+const onButtonClicked = (pressedValue) => {
+    // Handle the click event. You might want to do something with the numberOrString.
+    console.log('Button clicked with:', pressedValue);
+    keyboardValue.value = keyboardValue.value * 10 + pressedValue * 1
+    console.log(keyboardValue.value)
+}
 </script>
 
 <template>
     <div class="grid grid-cols-3 gap-1 bg-grey_darker p-1">
         <div
-            v-for="number in [1,2,3,4,5,6,7,8,9, 0, 'del', 'undo']"
-            class="flex justify-center items-center text-2xl font-display text-white bg-grey_lighterer rounded-lg"
+            v-for="number in ['1','2','3','4','5','6','7','8','9',  'del', '0', 'undo']"
+            class="flex justify-center items-center bg-grey_lighterer rounded-lg hover:bg-red hover:cursor-pointer"
         >
-            {{number}}
+            <KeyboardButton
+                :text="number"
+                @clicked="onButtonClicked"
+            />
         </div>
     </div>
 
