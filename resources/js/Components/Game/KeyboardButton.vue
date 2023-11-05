4d9fa62636b4d3fcfa56a26e4625331839377dc6
Tim Vande Walle <tim@vandewalle.mobi>
2023-11-05 13:27:27 +0100

    ✨ Add code to keyboard to handle button clicks

---
+++ resources/js/Components/Game/KeyboardButton.vue
@@ -0,0 +1,23 @@
+<script setup>
+/*
+import {FontAwesomeIcon} from '@fortawesome/vue-fontawesome'
+import {faXmark} from "@fortawesome/free-solid-svg-icons";
+*/
+
+const props = defineProps({
+    text: String
+})
+
+const emit = defineEmits(['clicked'])
+
+const handleClick = () => {
+    emit('clicked', props.text) // Emit the 'clicked' event with the number
+}
+
+</script>
+
+<template>
+    <div class="text-2xl font-display text-white" @click="handleClick">
+        {{text}}
+    </div>
+</template>
