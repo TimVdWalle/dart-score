45547f1818972b28fd856960f1e4f78516c227d6
Tim Vande Walle <tim@vandewalle.mobi>
2023-11-08 21:24:22 +0100

    ⚡ Update Show.vue to pass players to players component

--- resources/js/Pages/Game/Show.vue
+++ resources/js/Pages/Game/Show.vue
@@ -1,19 +1,24 @@
 <script setup>
-import {ref} from 'vue';
-import {Head} from '@inertiajs/vue3';
+import { ref } from 'vue';
+import { Head, usePage } from '@inertiajs/vue3';
 import Players from "@/Components/Game/Players.vue";
 
 import GameLayout from "@/Layouts/GameLayout.vue";
 import Keyboard from "@/Components/Game/Keyboard.vue";
+
+const { props } = usePage();
+const players = ref(props.game.players || []);
 </script>
 
 <template>
     <Head title="Game"/>
 
-    <GameLayout classes=" " style="background-color: blue">
-        <Players class="flex h-[40vh] min-h-[100px] bg-white justify-center items-center bg-white"/>
+    <GameLayout>
+        <Players
+            :players="players"
+            class="flex h-[40vh] min-h-[100px] bg-white justify-center items-center"/>
 
-        <Keyboard class="fixed bottom-0 w-full h-[30vh] min-h-[250px]"/>
+        <Keyboard class="fixed bottom-0 w-full h-[40vh] min-h-[250px]"/>
 
     </GameLayout>
 </template>
