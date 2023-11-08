b0db8340adfd1f8f35983cba0bcc173cd8d6cb5b
Tim Vande Walle <tim@vandewalle.mobi>
2023-11-08 21:24:59 +0100

    💄 Update UI of keyboard and buttons

--- resources/js/Components/Game/KeyboardButton.vue
+++ resources/js/Components/Game/KeyboardButton.vue
@@ -1,8 +1,10 @@
 <script setup>
+import {ref} from 'vue';
 import {faUserPlus} from "@fortawesome/free-solid-svg-icons";
 import Button from "@/Components/CardSmall.vue";
 import {FontAwesomeIcon} from "@fortawesome/vue-fontawesome";
 import {faDeleteLeft} from "@fortawesome/free-solid-svg-icons";
+import {faArrowRightLong} from "@fortawesome/free-solid-svg-icons";
 
 const props = defineProps({
     button: Object
@@ -10,14 +12,18 @@ const props = defineProps({
 
 const emit = defineEmits(['clicked'])
 
+const isActive = ref(false);
+
 const handleClick = () => {
-    emit('clicked', props.button) // Emit the 'clicked' event with the number
+    isActive.value = true; // Set active state to true when clicked
+    emit('clicked', props.button) // Emit the 'clicked' event with the button object
+    setTimeout(() => isActive.value = false, 100); // Reset active state after a short delay
 }
 
 </script>
 
 <template>
-    <div class="h-full w-full flex justify-center items-center bg-grey_lighterer rounded-lg hover:bg-red hover:cursor-pointer text-2xl font-display text-white" @click="handleClick">
+    <div :class="{'bg-red': isActive}" @click="handleClick" class="h-full w-full flex justify-center items-center bg-grey_lighterer rounded-lg hover:cursor-pointer text-2xl font-display text-white">
         <span v-if="button.type === 'number'">
         {{button.value}}
         </span>
@@ -27,5 +33,17 @@ const handleClick = () => {
         <span v-else-if="button.value === 'faDeleteLeft'">
             <font-awesome-icon :icon="faDeleteLeft" class="fa-1x text-white"/>
         </span>
+        <span v-else-if="button.value === 'faArrowRightLong'">
+            <font-awesome-icon :icon="faArrowRightLong" class="fa-1x text-white"/>
+        </span>
     </div>
 </template>
+
+<style>
+/* Use media queries to apply hover styles only for non-touch devices */
+@media (hover: hover) {
+    .hover:bg-red:hover {
+        background-color: red;
+    }
+}
+</style>
