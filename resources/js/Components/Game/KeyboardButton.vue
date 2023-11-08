f41056e200a03ed097fd221998b3825068ce8404
Tim Vande Walle <tim@vandewalle.mobi>
2023-11-08 21:23:54 +0100

    ⚡ Update keyboard

--- resources/js/Components/Game/KeyboardButton.vue
+++ resources/js/Components/Game/KeyboardButton.vue
@@ -1,23 +1,31 @@
 <script setup>
-/*
-import {FontAwesomeIcon} from '@fortawesome/vue-fontawesome'
-import {faXmark} from "@fortawesome/free-solid-svg-icons";
-*/
+import {faUserPlus} from "@fortawesome/free-solid-svg-icons";
+import Button from "@/Components/CardSmall.vue";
+import {FontAwesomeIcon} from "@fortawesome/vue-fontawesome";
+import {faDeleteLeft} from "@fortawesome/free-solid-svg-icons";
 
 const props = defineProps({
-    text: String
+    button: Object
 })
 
 const emit = defineEmits(['clicked'])
 
 const handleClick = () => {
-    emit('clicked', props.text) // Emit the 'clicked' event with the number
+    emit('clicked', props.button) // Emit the 'clicked' event with the number
 }
 
 </script>
 
 <template>
-    <div class="text-2xl font-display text-white" @click="handleClick">
-        {{text}}
+    <div class="h-full w-full flex justify-center items-center bg-grey_lighterer rounded-lg hover:bg-red hover:cursor-pointer text-2xl font-display text-white" @click="handleClick">
+        <span v-if="button.type === 'number'">
+        {{button.value}}
+        </span>
+        <span v-else-if="button.value === 'faDeleteLeft'">
+            <font-awesome-icon :icon="faDeleteLeft" class="fa-1x text-white"/>
+        </span>
+        <span v-else-if="button.value === 'faDeleteLeft'">
+            <font-awesome-icon :icon="faDeleteLeft" class="fa-1x text-white"/>
+        </span>
     </div>
 </template>
