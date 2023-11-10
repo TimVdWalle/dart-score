11e9c7c6af89369bb187bb8f15c874dfe35e2885
Tim Vande Walle <tim@vandewalle.mobi>
2023-11-10 23:03:38 +0100

    Add $game to mapPlayers

--- app/Contracts/GameTypes/Game101Type.php
+++ app/Contracts/GameTypes/Game101Type.php
@@ -5,13 +5,14 @@ namespace App\Contracts\GameTypes;
 use App\Contracts\GameTypeInterface;
 use App\Enums\GameType;
 use App\Factories\GameTypeFactory;
+use App\Models\Game\Game;
 use App\Models\Game\Player;
 use Illuminate\Support\Collection;
 
 class Game101Type extends AbstractGameType {
-    public function initializeScores(Collection $players): Collection {
+    public function initializeScores(Collection $players, Game $game): Collection {
         $initialScore = GameType::Game101->getStartingScore();
-        return GameTypeFactory::mapPlayers($players, $initialScore);
+        return GameTypeFactory::mapPlayers($players, $game, $initialScore);
     }
 
     // Implement other unique methods for Game101Type
--- app/Contracts/GameTypes/Game501Type.php
+++ app/Contracts/GameTypes/Game501Type.php
@@ -2,17 +2,21 @@
 
 namespace App\Contracts\GameTypes;
 
-use App\Contracts\GameTypeInterface;
-use App\Contracts\OutTypeStrategyInterface;
 use App\Enums\GameType;
 use App\Factories\GameTypeFactory;
-use App\Models\Game\Player;
+use App\Models\Game\Game;
 use Illuminate\Support\Collection;
 
 class Game501Type extends AbstractGameType {
-    public function initializeScores(Collection $players): Collection {
+    /**
+     * @param Collection $players
+     * @param Game $game
+     * @return Collection
+     */
+    public function initializeScores(Collection $players, Game $game): Collection
+    {
         $initialScore = GameType::Game501->getStartingScore();
-        return GameTypeFactory::mapPlayers($players, $initialScore);
+        return GameTypeFactory::mapPlayers($players, $game, $initialScore);
     }
 
     // Implement other unique methods for Game501Type
