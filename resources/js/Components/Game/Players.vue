1622d6a1b71923c24f1ebc433fc5e55323920434
Tim Vande Walle <tim@vandewalle.mobi>
2023-11-11 18:16:46 +0100

    ⚡ Update UI for player table : scrollable in fixed height

--- resources/js/Components/Game/Players.vue
+++ resources/js/Components/Game/Players.vue
@@ -8,12 +8,85 @@ const props = defineProps({
 </script>
 
 <template>
-    <div class="flex ">
-        <div v-if="props.players.length > 0" class="flex justify-center items-center bg-red w-full h-full">
-            {{ props.players[0].name }} <!-- Display the name of the first player -->
-        </div>
-        <div v-if="props.players.length > 1" class="flex justify-center items-center bg-blue w-full h-full">
-            {{ props.players[1].name }} <!-- Display the name of the second player -->
+    <div class="table-container">
+        <table class="nk-table w-full">
+            <thead>
+            <tr class="headingrow">
+                <th colspan="3">Skills Table</th>
+            </tr>
+            <tr>
+                <th>Name</th>
+                <th>Game Line</th>
+                <th class="text-center">Percent</th>
+            </tr>
+            </thead>
+        </table>
+        <div class="tbody-container">
+            <table class="nk-table w-full">
+                <tbody>
+                <tr>
+                    <td>Duke</td>
+                    <td>Top</td>
+                    <td class="text-center"><strong>85</strong></td>
+                </tr>
+                <tr>
+                    <td>Duke</td>
+                    <td>Top</td>
+                    <td class="text-center"><strong>85</strong></td>
+                </tr>
+                <tr>
+                    <td>Duke</td>
+                    <td>Top</td>
+                    <td class="text-center"><strong>85</strong></td>
+                </tr>
+                <tr>
+                    <td>Duke</td>
+                    <td>Top</td>
+                    <td class="text-center"><strong>85</strong></td>
+                </tr>
+                <tr>
+                    <td>Duke</td>
+                    <td>Top</td>
+                    <td class="text-center"><strong>85</strong></td>
+                </tr>
+                <tr>
+                    <td>Duke</td>
+                    <td>Top</td>
+                    <td class="text-center"><strong>85</strong></td>
+                </tr>
+                <tr>
+                    <td>Duke</td>
+                    <td>Top</td>
+                    <td class="text-center"><strong>85</strong></td>
+                </tr>
+                <tr>
+                    <td>Duke</td>
+                    <td>Top</td>
+                    <td class="text-center"><strong>85</strong></td>
+                </tr></tbody>
+            </table>
         </div>
     </div>
 </template>
+
+<style>
+.table-container {
+    display: block;
+}
+
+.nk-table {
+    width: 100%;
+    table-layout: fixed;
+}
+
+.tbody-container {
+    max-height: 300px; /* Adjust this height as needed */
+    overflow-y: auto;
+    display: block;
+}
+
+.tbody-container table {
+    margin-top: -1px; /* To align with the header */
+}
+</style>
+
