2e79fac439ea71f69e3060e8f5b39e28cf555e2b
Tim Vande Walle <tim@vandewalle.mobi>
2023-11-19 00:02:41 +0100

    🔨 Refactor project for dependency injections

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use App\Services\PlayerService;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -11,7 +12,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        // Bind PlayerService to a closure that resolves it from the container
+        $this->app->bind(PlayerService::class, function ($app) {
+            return new PlayerService(/* constructor arguments if any */);
+        });
     }
 
     /**
