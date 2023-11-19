a6ea05969e9eb1508de10043db6c262cf5f9f143
Tim Vande Walle <tim@vandewalle.mobi>
2023-11-19 19:18:14 +0100

    🔨 Refactor gamecontroller to create hash at store instead of at init

--- tests/Feature/GameControllerTest.php
+++ tests/Feature/GameControllerTest.php
@@ -1,9 +1,17 @@
 <?php
 
 use App\Models\Game;
+use App\Services\GameService;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
 uses(Tests\TestCase::class, RefreshDatabase::class);
+
+beforeEach(function () {
+    // Mock the GameService to control the hash generation
+    $this->gameServiceMock = Mockery::mock(GameService::class);
+    $this->app->instance(GameService::class, $this->gameServiceMock);
+});
+
 
 it('initializes game correctly', function () {
     $response = $this->get(route('game.init'));
@@ -17,7 +25,7 @@ it('initializes game with correct view and data', function () {
     $response->assertStatus(200)
         ->assertInertia(fn ($assert) => $assert
             ->component('Game/Init')
-            ->has('gameHash')
+//            ->has('gameHash')
             ->has('csrf')
         );
 });
@@ -25,49 +33,79 @@ it('initializes game with correct view and data', function () {
 it('fails to store game with invalid data', function () {
     $response = $this->post(route('game.store'), []);
 
-    $response->assertSessionHasErrors(['hash', 'gameType', 'outType', 'players']);
+    $response->assertSessionHasErrors(['gameType', 'outType', 'players']);
 });
 
 it('successfully stores a new game and redirects', function () {
+    $mockedHash = 'testhash'; // Mocked hash value
+
+    $this->gameServiceMock
+        ->shouldReceive('getNextHash')
+        ->once()
+        ->andReturn($mockedHash);
+
+    // Mock the createGame method to return a Game instance
+    $this->gameServiceMock
+        ->shouldReceive('createGame')
+        ->once()
+        ->andReturnUsing(function ($hash, $gameType, $outType, $players) {
+            $game = new Game();
+            $game->hash = $hash;
+            $game->game_type = $gameType;
+            $game->out_type = $outType;
+            $game->save();
+
+            // Additional logic to add players to the game if needed
+
+            return $game;
+        });
+
     $gameData = [
-        'hash' => 'testhash',
-        'gameType' => '501',  // Example game type
-        'outType' => 'double_exact',  // Example out type
-        'players' => [
-            'Player 1',
-            'Player 2'
-        ],
+        'gameType' => '501',
+        'outType' => 'double_exact',
+        'players' => ['Player 1', 'Player 2'],
     ];
 
     $response = $this->post(route('game.store'), $gameData);
 
-    $response->assertRedirect(route('game.show', ['gameHash' => 'testhash']));
+    $response->assertRedirect(route('game.show', ['gameHash' => $mockedHash]));
 });
 
 it('stores new game successfully', function () {
-    // Mock data similar to what your form would send
+    $mockedHash = '123456'; // Mocked hash value
+
+    $this->gameServiceMock
+        ->shouldReceive('getNextHash')
+        ->once()
+        ->andReturn($mockedHash);
+
+    // Assuming createGame method does some processing and then saves the game
+    $this->gameServiceMock
+        ->shouldReceive('createGame')
+        ->once()
+        ->andReturnUsing(function ($hash, $gameType, $outType, $players) {
+            $game = new Game();
+            $game->hash = $hash;
+            $game->game_type = $gameType;
+            $game->out_type = $outType;
+            $game->save();
+
+            return $game;
+        });
+
     $gameData = [
-        'hash' => '123456',  // Example hash value
-        'gameType' => '501', // Assuming '501' is a valid game type
-        'outType' => 'double_exact', // Assuming this is a valid out type
-        'players' => [
-            'Player 1',
-            'Player 2'
-        ],
+        'gameType' => '501',
+        'outType' => 'double_exact',
+        'players' => ['Player 1', 'Player 2'],
     ];
 
     $response = $this->post(route('game.store'), $gameData);
 
-    // Fetch the game based on the hash used in `$gameData`
-    $game = Game::where('hash', $gameData['hash'])->first();
+    // Since we're mocking, we assert that the redirect happens as expected
+    // The actual creation of the game in the database is not performed by the mock
+    $response->assertRedirect(route('game.show', ['gameHash' => $mockedHash]));
 
-    // Assert redirection to the correct route with the right game hash
-    $response->assertRedirect(route('game.show', ['gameHash' => $game->hash]));
-
-    // Assert that a game was indeed created
-    expect(Game::count())->toBe(1);
-
-    // Additional assertions can be made here to verify the game data
+    // Additional assertions can be added here if needed
 });
 
 //it('displays the correct game data', function () {
@@ -84,7 +122,6 @@ it('stores new game successfully', function () {
 
 it('redirects if the game does not exist', function () {
     $response = $this->get(route('game.show', ['gameHash' => 'nonexistent']));
-
     $response->assertRedirect(route('game.init'));
 });
 
