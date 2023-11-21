aba189ad0284f632e238bb31eb843993c6e3586a
Tim Vande Walle <tim@vandewalle.mobi>
2023-11-21 22:55:44 +0100

    ✨ Add saving of score

--- app/Services/ScoreService.php
+++ app/Services/ScoreService.php
@@ -2,12 +2,59 @@
 
 namespace App\Services;
 
+use App\Factories\GameTypeFactory;
 use App\Models\Game;
 use App\Models\Player;
 use App\Models\Score;
+use Exception;
 
 class ScoreService
 {
+    /**
+     * @var GameplayService
+     */
+    protected $gameplayService;
+
+    public function __construct(GameplayService $gameplayService)
+    {
+        $this->gameplayService = $gameplayService;
+    }
+
+    /**
+     * @throws Exception
+     */
+    public function handleScoreSubmission(Game $game, int $playerId, int $score): \Illuminate\Http\JsonResponse
+    {
+        $currentPlayer = $this->gameplayService->determineCurrentTurn($game);
+        $currentSet = $this->gameplayService->getCurrentSet($game);
+        $currentLeg = $this->gameplayService->getCurrentLeg($game);
+
+        if (!$currentPlayer || $currentPlayer->id != $playerId) {
+            throw new Exception('Invalid player turn');
+        }
+
+        if (!$currentSet) {
+            throw new Exception('Invalid current set');
+        }
+
+        if (!$currentLeg) {
+            throw new Exception('Invalid current leg');
+        }
+
+        $gameTypeObject = GameTypeFactory::create($game);
+        $isValid = $gameTypeObject->validateScore($game, $currentPlayer, $score, $currentSet, $currentLeg);
+
+        if($isValid){
+            $this->save($game, $currentPlayer, $score, $currentSet->id, $currentLeg->id);
+        }
+
+
+        // Check for a winner
+//        if ($gameTypeObject->checkWinner()) {
+//            return 'We have a winner!';
+//        }
+        return response()->json(['message' => 'Score accepted'], 201);
+    }
 
     /**
      * @param Game $game
