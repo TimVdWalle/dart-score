12b6ebfb9d294bbdbf6842dab4e731db78b9d052
Tim Vande Walle <tim@vandewalle.mobi>
2023-11-21 20:37:56 +0100

    ✨ Add leg and set

--- app/Services/ScoreService.php
+++ app/Services/ScoreService.php
@@ -8,21 +8,24 @@ use App\Models\Score;
 
 class ScoreService
 {
+
     /**
      * @param Game $game
      * @param Player $player
      * @param int $score
+     * @param int $setId
+     * @param int $legId
      * @return Score
      */
-    public function save(Game $game, Player $player, int $score): Score
+    public function save(Game $game, Player $player, int $score, int $setId, int $legId): Score
     {
-        $score = Score::create([
+        return Score::create([
             'game_id' => $game->id,
             'player_id' => $player->id,
+            'set_id' => $setId,
+            'leg_id' => $legId,
             'score' => $score,
         ]);
-
-        return $score;
     }
 
 }
